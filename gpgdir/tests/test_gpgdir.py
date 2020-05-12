fff4af8720fa7997fe64d64dbd04763e2095d02d
Fabrice Sommavilla <flasomm@gmail.com>
2020-05-12 23:59:00 +0200

    Mock unit test wip

--- gpgdir/tests/test_gpgdir.py
+++ gpgdir/tests/test_gpgdir.py
@@ -11,7 +11,15 @@ class TestGpgDir(unittest.TestCase):
 
     @mock.patch('gpgdir.get_home_dir')
     @mock.patch('gpgdir.get_gpg_dir')
-    def test_get_gpg_dir_exists(self, mock_get_home_dir, mock_get_gpg_dir):
+    def test_get_gpg_dir_exists(self, mock_get_gpg_dir, mock_get_home_dir):
+        mock_get_home_dir.return_value = '/abc'
+        mock_get_gpg_dir.return_value = '/abc/.gnupg'
+        home = gpgdir.get_home_dir()
+        self.assertEqual(gpgdir.get_gpg_dir(), home + "/.gnupg")
+
+    @mock.patch('gpgdir.get_home_dir')
+    @mock.patch('gpgdir.get_gpg_dir')
+    def test_get_gpg_dir_not_exists(self, mock_get_gpg_dir, mock_get_home_dir):
         mock_get_home_dir.return_value = '/abc'
         mock_get_gpg_dir.return_value = '/abc/.gnupg'
         home = gpgdir.get_home_dir()
